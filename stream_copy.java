4d8c27c3a4281441f755d25a93b40baf2bb74c09
ZacharyForman <au.zachary.forman@gmail.com>
2015-01-17 19:07:02 +1100

    Java stream copying utility code.

---
+++ stream_copy.java
@@ -0,0 +1,23 @@
+/**
+ * Class that provides miscellaneous utilities.
+ */
+class Utils {
+  
+  private final static int BUFFER_LENGTH = 4096;
+
+  /**
+   * Copies from an input stream to an output stream.
+   * The caller must handle any exceptions thrown.
+   *
+   * @param in The stream to copy from.
+   * @param out The stream to copy to.
+   */
+  public static void streamCopy(final InputStream in, final OutputStream out) throws IOException {
+    char[] buffer = new char[BUFFER_LENGTH];
+    int length;
+    while ((length = in.read(buffer)) != -1) {
+      out.write(buffer, length);
+    }
+  }
+
+}
